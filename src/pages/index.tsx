b13c48675bddcb450d42a1cc9e91496126d68b3d
Prakhar Vohra <webfirex@gmail.com>
2024-11-30 19:59:35 +0530

    Updates

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -68,7 +68,7 @@ export default function Home() {
       <MantineProvider>
         <CardsCarousel />
       </MantineProvider>
-      <StickyBtn />
+      {/* <StickyBtn /> */}
     </main>
   );
 }
